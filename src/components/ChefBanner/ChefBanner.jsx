d39ca97561301c1c363875205abdb02d6f57f998
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-05-03 23:22:33 +0600

    Banner for chefs recipe page done

---
+++ src/components/ChefBanner/ChefBanner.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import "./ChefBanner.css";
+const ChefBanner = ({ chef }) => {
+  return (
+    <div>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="pic">
+          <img className="img-fluid" src={chef?.image} alt="" />
+        </div>
+        <div className="des ms-5">
+          <h1 className="mb-4">{chef?.name}</h1>
+          <p>
+            <span className="bold">short bio/description:</span> {chef?.bio}
+          </p>
+          <p>
+            <span className="bold">Likes:</span> {chef?.likes}
+          </p>
+          <p>
+            <span className="bold">Number of recipes:</span>{" "}
+            {chef?.numbers_of_recipes}
+          </p>
+          <p>
+            <span className="bold">Years of experience:</span>{" "}
+            {chef?.years_of_experience}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChefBanner;
--- src/pages/ChefRecipes/ChefRecipes.jsx
+++ src/pages/ChefRecipes/ChefRecipes.jsx
@@ -1,9 +1,35 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './ChefRecipes.css'
+import { useState } from 'react';
+import ChefBanner from '../../components/ChefBanner/ChefBanner';
 
 const ChefRecipes = () => {
+    const { id } = useParams();
+    const [chef, setChef] = useState(null);
+    const [recipes, setRecipes] = useState([]);
+
+    console.log(id)
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/chefs/${id}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setChef(data);
+            });
+    }, [id]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/recipes/${id}}`)
+            .then((res) => res.json())
+            .then((data) => setRecipes(data));
+    }, [id]);
+
+
     return (
-        <div>
-            <h1>This is chef recipes</h1>
+        <div className='container'>
+            <ChefBanner chef={chef}></ChefBanner>
         </div>
     );
 };
