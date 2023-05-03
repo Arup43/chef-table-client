ee7c218ead3fe9de5339c0799513ad17a92bd4b7
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-05-03 18:48:25 +0600

    Added not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,13 @@
-import './NotFound.css'
+import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1>This is 404 page</h1>
-        </div>
-    );
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center not-found">
+      <h1>Sorry, Page not found!!</h1>
+      <br />
+      <img src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg"></img>
+    </div>
+  );
 };
 
 export default NotFound;
